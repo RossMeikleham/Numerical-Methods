e8b6f323dee267ad5497d74a559752b4724992ad
RossMeikleham <rossmeikleham@hotmail.co.uk>
2014-10-28 16:10:22 +0000

    Added module to generate vandemonde matrixies and their determinants

---
+++ src/Vandermonde.py
@@ -0,0 +1,36 @@
+#Vandermonde Matrix creation
+
+import numpy
+
+#Creates a n * n Vandermonde matrix 
+#given a list of n values. 
+def vandermonde_matrix(values):
+    rows = [[a ** i for i in range (0, len(values))] for a in values]
+    return numpy.matrix(rows)
+    
+#Calculate determinant of a vandermonde matrix
+def vandermonde_det(v):
+    v = v.transpose()
+    matrix = v.tolist()
+    
+    if len(matrix) == 1:
+        return 1
+
+    values = matrix[1] #Obtain initial vandermonde values
+    
+    det = 1
+    for i in range (0, len(values)):
+        for j in range (0, i):
+            det *= (values[i] - values[j])
+
+    return det
+
+
+#examples
+v = vandermonde_matrix([1,2,3,4])
+print(v)
+d = vandermonde_det(v)
+print(d)
+
+v = vandermonde_matrix([1])
+print(vandermonde_det(v))
