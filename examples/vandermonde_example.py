1872272a75d353aa03ce21b1e85a1c44b6865ccb
RossMeikleham <rossmeikleham@hotmail.co.uk>
2014-10-28 16:34:01 +0000

    Added setup file for building the package, created vandermonde example

---
+++ examples/vandermonde_example.py
@@ -0,0 +1,5 @@
+from numericalmethods.vandermonde import *
+
+v = vandermonde_matrix([1,2,3,4])
+print(v)
+print(vandermonde_det(v))
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='numericalmethods',
+      version='0.0.1',
+      description='Collection of modules which implement Numerical Methods concepts',
+      url='https://github.com/RossMeikleham/Numerical-Methods',
+      author='RossMeikleham',
+      author_email='RossMeikleham@hotmail.co.uk',
+      license='MIT',
+      package_dir={'numericalmethods': 'src'},
+      packages=['numericalmethods'],
+      zip_safe=False)
