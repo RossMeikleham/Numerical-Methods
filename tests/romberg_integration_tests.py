001f48e32e6a24600b53945a49b51e9073c5db79
RossMeikleham <rossmeikleham@hotmail.co.uk>
2014-10-28 18:33:40 +0000

    Added test suite for romberg integration

---
+++ tests/romberg_integration_tests.py
@@ -0,0 +1,36 @@
+import unittest
+from numericalmethods.romberg import *
+from math import log
+
+class TestsRombergIntegration(unittest.TestCase):
+    
+    #Test integration with e^x
+    def test_exp(self):
+        fn = lambda x : exp(x)
+        a = 1
+        b = 1
+        self.assertEqual(romberg(a, b, fn, 10), 0)
+
+        fn = lambda x : exp(x)
+        a = 0
+        b = 1
+        self.assertAlmostEqual(romberg(a, b, fn, 10), exp(1) - 1, places = 10)
+
+
+    #Test integration of constant expression
+    def test_const(self):
+        fn = lambda x : 1
+        a = 32
+        b = 50
+        self.assertEqual(romberg(a, b, fn, 10), b - a)
+
+   #Test integration of 1/x
+    def test_log(self):
+        fn = lambda x : 1/x #integration of 1/x = log(x)
+        a = 5
+        b = 10
+        self.assertAlmostEqual(romberg(a, b , fn, 10), log(2))
+
+
+if __name__ == '__main__':
+    unittest.main()
