ec0d9a116f53b2e428ba274f536075da722e53c7
RossMeikleham <rossmeikleham@hotmail.co.uk>
2014-11-26 18:47:21 +0000

    Added example for linear equation solver

---
+++ examples/linear_solver_example.py
@@ -0,0 +1,18 @@
+
+from numericalmethods.linear_equations import *
+
+a = numpy.matrix([[8.0, 1.0, -1.0], [2.0, 1.0, 9.0], [1.0, -7.0, 2.0]])
+print(lu_decomposition(a))
+
+
+# Equations: 
+# 8 = 8x + y - Z
+# 12 = 2x + y + 9Z
+# -4 = x - 7y + 2z
+
+ax = numpy.matrix([[8.0, 1.0, -1.0], [2.0, 1.0, 9.0], [1.0, -7.0, 2.0]])
+b = [8.0, 12.0, -4.0]
+
+print(solve_eqns(ax, b))
+
+
